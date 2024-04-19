5e49d845706c49f24a590ef633129981a816d750
Alexis Schaffer <contact@alexisschaffer.me>
2024-04-19 18:36:45 +0200

    Fix DB schema

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS weather_data (
     id UUID primary key NOT NULL,
     timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    provider VARCHAR(50) NOT NULL,
+    provider SMALLINT NOT NULL,
     temperature DOUBLE PRECISION NOT NULL,
     temperature_unit VARCHAR(10) NOT NULL,
     pressure DOUBLE PRECISION NOT NULL,
