aa984c390b3ab0b5208a72f6f61d98326ff8b95b
Alexis Schaffer <contact@alexisschaffer.me>
2024-04-19 20:33:59 +0200

    Add ApiService

---
+++ ApiService/Entities/TemperatureUnit.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiService.Entities
+{
+    public enum TemperatureUnit
+    {
+        Celsius = 0,
+        Fahrenheit = 1
+    }
+}
---
+++ ApiService/Program.cs
@@ -0,0 +1,40 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // For EntityFramework
+            builder.Services.AddDbContext<AppDbContext>(
+                options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
